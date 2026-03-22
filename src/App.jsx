30d1943a22f4f0a1d0ca1e3a75055e122c64a827
Ozan Halis Demiralp <120143887+OzanDemiralp@users.noreply.github.com>
2026-03-22 23:03:42 +0300

    fix backend connection and resium display issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import { Color } from 'cesium';
 import 'cesium/Build/Cesium/Widgets/widgets.css';
+import { PointGraphics } from 'resium';
 import { Viewer, Entity } from 'resium';
 import { useSatelliteTracker } from './hooks/useSatelliteTracker';
+import { useEffect, useRef } from 'react';
 
 function App() {
   const { position, error } = useSatelliteTracker('stations', 'ISS (ZARYA)');
+  const viewerRef = useRef(null);
+
+  useEffect(() => {
+    const viewer = viewerRef.current?.cesiumElement;
+    if (viewer && viewer.infoBox) {
+      viewer.infoBox.frame.sandbox =
+        'allow-same-origin allow-top-navigation allow-pointer-lock allow-popups allow-forms allow-scripts';
+    }
+  }, []);
 
   if (error)
     return (
@@ -12,7 +23,7 @@ function App() {
     );
 
   return (
-    <Viewer full>
+    <Viewer full ref={viewerRef}>
       {position && (
         <Entity position={position} name='ISS (ZARYA)'>
           <PointGraphics pixelSize={15} color={Color.LIME} />
